524c40ccf145e7443ebc5134112269193a06c11f
j9794 <juanmanuel9794@hotmail.com>
2016-03-17 19:29:16 -0300

    Trying to start modularization of the code

--- src/main/scala/org/uqbar/thin/ide/Core.scala
+++ src/main/scala/org/uqbar/thin/ide/Core.scala
@@ -4,15 +4,14 @@ import java.util.jar.JarFile
 import scala.reflect.internal.util.ScalaClassLoader
 import scala.collection.JavaConversions._
 import java.util.jar.JarEntry
+import java.net.URL
+import java.net.URLClassLoader
+import scala.reflect.internal.util.ScalaClassLoader.URLClassLoader
+import java.io.File
+
 
 
 object Core extends App{
-  
-  def ejecutarPlugin:Class[_]=>Unit = { x => print(x)
-                                   print("\n") 
-                                 }
-
-  val paths = io.Source.fromFile("config.txt").getLines
   
   implicit class JarClass(entry:JarEntry){
     val (className,typeSuffix) = {
@@ -21,14 +20,29 @@ object Core extends App{
     }
     def isClass:Boolean = !entry.isDirectory() && (typeSuffix equals ".class")
   }
+  
+  def executePlugin:Class[_]=>Unit = { x => print(x)
+                                  //placeholder
+                                   print("\n") 
+                                 }
+  
+  val paths = io.Source.fromFile("config.txt").getLines
     
   val classes = for{path <- paths
-      file <- new java.io.File(path).listFiles if file.getName endsWith ".jar"
+      file <- SourceFinder(path)
+      classLoader = new ScalaClassLoader.URLClassLoader(List(file.toURI.toURL),getClass.getClassLoader)
       jarContents = new JarFile(file).entries
       entry<-jarContents
       if entry.isClass
-      }yield{getClass.getClassLoader.loadClass(entry.className)}
-       
-  classes.foreach { ejecutarPlugin }
+      }yield{classLoader.loadClass(entry.className)}
+  classes.foreach { executePlugin }
+  
 }
 
+object SourceFinder{
+  def apply(path:String) = path match{
+    case path if isLocalDirectory(path) => new File(path).listFiles.filter(_.getName.endsWith(".jar")).toSeq
+    case _ => List()
+  }
+  def isLocalDirectory(path:String) = path.startsWith("C")
+}
